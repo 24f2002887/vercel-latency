b91481eea54ec08788b0c86bf1742ee12e5c8a60
Jaismeen Kaur <24f2002887@ds.study.iitm.ac.in>
2025-10-04 12:39:10 +0000

    initial commit

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+app = FastAPI()
+
+app.add_middleware(
+    
+)
